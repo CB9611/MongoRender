a8a09b83654b5176146d49cdf0ead22be8d3eb60
Cameron <cameron.branch@selu.edu>
2024-03-04 16:06:13 -0600

    Change5

--- testmongo.js
+++ testmongo.js
@@ -1,7 +1,7 @@
 const { MongoClient } = require("mongodb");
 
 // The uri string must be the connection string for the database (obtained on Atlas).
-const uri = "mongodb+srv://cb9611:cmps415@cluster0.xvm65mx.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const uri = "mongodb+srv://testUser:cmps415@cluster0.xvm65mx.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
 // --- This is the standard stuff to get it to work on the browser
 const express = require('express');
