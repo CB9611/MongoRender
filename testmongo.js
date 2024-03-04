d36af807ee1cf6ec43de3d5fefae19b7f7144e56
Cameron <cameron.branch@selu.edu>
2024-03-04 14:52:49 -0600

    Change1

--- testmongo.js
+++ testmongo.js
@@ -1,7 +1,7 @@
 const { MongoClient } = require("mongodb");
 
 // The uri string must be the connection string for the database (obtained on Atlas).
-const uri = "mongodb+srv://<user>:<password>@ckmdb.5oxvqja.mongodb.net/?retryWrites=true&w=majority";
+const uri = "mongodb+srv://CB9611:Computerscience415@cluster0.tpx7g8s.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
 // --- This is the standard stuff to get it to work on the browser
 const express = require('express');
@@ -16,14 +16,13 @@ app.use(express.urlencoded({ extended: true }));
 // routes will go here
 
 // Default route:
-app.get('/', function(req, res) {
-  res.send('Starting... ');
+app.get('/', function(res) {
+  res.send('Starting...');
 });
 
 app.get('/say/:name', function(req, res) {
   res.send('Hello ' + req.params.name + '!');
 });
-
 
 // Route to access database:
 app.get('/api/mongo/:item', function(req, res) {
@@ -33,8 +32,8 @@ console.log("Looking for: " + searchKey);
 
 async function run() {
   try {
-    const database = client.db('ckmdb');
-    const parts = database.collection('cmps415');
+    const database = client.db('DatabaseExample');
+    const parts = database.collection('MyStuff');
 
     // Hardwired Query for a part that has partID '12345'
     // const query = { partID: '12345' };
